3e9f18581c4ad53e8d1b2d66302c375016d70034
Andrew Werner <awerner32@gmail.com>
2019-04-27 12:30:34 -0400

    basically works

--- tdigestgo.go
+++ tdigestgo.go
@@ -1,19 +1,26 @@
 package tdigestgo
 
-import "sync"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"sort"
+	"sync"
+	"sync/atomic"
+)
 
 type centroid struct {
 	mean, count float64
 }
 
-type centroids []centroid
+const locked = math.MinInt64
 
 // TDigest approximates a distribution of floating point numbers.
 type TDigest struct {
 	config
 
-	// numUnmerged is accessed with atomics
-	numUnmerged int64
+	// unmergedIdx is accessed with atomics
+	unmergedIdx int64
 
 	centroids centroids
 
@@ -23,24 +30,183 @@ type TDigest struct {
 
 		// merged is the size of the prefix of centroid used for the merged sorted
 		// data.
-		numMerged int
+		numMerged   int
+		mergedCount float64
 	}
 }
 
-func (td *TDigest) mergedSlice() centroids {
-	return td.centroids[:td.numMerged]
-}
-
-func NewTDigest(options ...Option) *TDigest {
+func New(options ...Option) *TDigest {
 	var td TDigest
-	defaultOptions.apply(&cfg)
-	optionList(options).apply(&cfg)
-	return &TDigest{
+	defaultOptions.apply(&td.config)
+	optionList(options).apply(&td.config)
+	td.mu.L = td.mu.RLocker()
+	td.centroids = make(centroids, capFromCompression(td.compression))
+	return &td
+}
 
-		compression: cfg.compression,
-		centroids:   make(centroids, cap),
+func capFromCompression(compression float64) int {
+	return (6 * (int)(compression)) + 10
+}
+
+func (td *TDigest) Record(mean float64) { td.Add(mean, 1) }
+
+func (td *TDigest) Add(mean, count float64) {
+	td.mu.RLock()
+	defer td.mu.RUnlock()
+	td.centroids[td.getAddIndexRLocked()] = centroid{mean: mean, count: count}
+}
+
+func (td *TDigest) Merge(other *TDigest) {
+	td.mu.Lock()
+	defer td.mu.Unlock()
+	other.mu.Lock()
+	defer other.mu.Unlock()
+	other.mergeLocked()
+	perm := rand.Perm(other.mu.numMerged)
+	for _, i := range perm {
+		td.centroids[td.getAddIndexLocked()] = other.centroids[i]
 	}
 }
+
+func (td *TDigest) getAddIndexRLocked() int {
+	for {
+		idx := int(atomic.AddInt64(&td.unmergedIdx, 1)) - 1
+		if idx < len(td.centroids) {
+			return idx
+		} else if idx == len(td.centroids) {
+			func() {
+				td.mu.RUnlock()
+				defer td.mu.RLock()
+				td.merge()
+				td.mu.Broadcast()
+			}()
+		} else {
+			td.mu.Wait()
+		}
+	}
+}
+
+func (td *TDigest) getAddIndexLocked() int {
+	for {
+		idx := int(atomic.AddInt64(&td.unmergedIdx, 1)) - 1
+		if idx < len(td.centroids) {
+			return idx
+		} else {
+			td.mergeLocked()
+		}
+	}
+}
+
+// TODO(ajwerner): optimize reading by storing CDF instead PDF
+
+func (td *TDigest) ValueAt(q float64) float64 {
+	td.merge()
+	td.mu.RLock()
+	defer td.mu.RUnlock()
+	return td.valueAtRLocked(q)
+}
+
+func isVerySmall(v float64) bool {
+	return !(v > .000000001 || v < -.000000001)
+}
+
+func (td *TDigest) valueAtRLocked(q float64) float64 {
+	if q < 0 || q > 1 {
+		panic(fmt.Errorf("invalid quantile %v", q))
+	}
+	// TODO: optimize this to be O(log(merged)) instead of O(merged)
+	goal := q * td.mu.mergedCount
+	k := 0.0
+	i := 0
+	for i = 0; i < td.mu.numMerged-1; i++ {
+		if k+td.centroids[i].count > goal {
+			break
+		}
+		k += td.centroids[i].count
+	}
+	n := &td.centroids[i]
+	deltaK := goal - k - (n.count / 2)
+	if isVerySmall(deltaK) {
+		return n.mean
+	}
+	right := deltaK > 0
+	// At the very extremes
+	if (right && ((i + 1) == td.mu.numMerged)) || (!right && i == 0) {
+		return n.mean
+	}
+	var nl, nr *centroid
+	if right {
+		nl = n
+		nr = &td.centroids[i+1]
+		k += n.count / 2
+	} else {
+		nl = &td.centroids[i-1]
+		nr = n
+		k -= n.count / 2
+	}
+	x := goal - k
+	m := (nr.mean - nl.mean) / ((nl.count / 2) + (nr.count / 2))
+	return m*x + nl.mean
+}
+
+func (td *TDigest) merge() {
+	td.mu.Lock()
+	defer td.mu.Unlock()
+	td.mergeLocked()
+}
+
+func (td *TDigest) mergeLocked() {
+	idx := int(atomic.LoadInt64(&td.unmergedIdx))
+	if idx > len(td.centroids) {
+		idx = len(td.centroids)
+	}
+	centroids := td.centroids[:idx]
+	sort.Sort(centroids)
+	totalCount := centroids.totalCount()
+	denom := 2 * math.Pi * totalCount * math.Log(totalCount)
+	normalizer := td.compression / denom
+	cur := 0
+	countSoFar := 0.0
+	for i := 1; i < len(centroids); i++ {
+		proposedCount := centroids[cur].count + centroids[i].count
+		z := proposedCount * normalizer
+		q0 := countSoFar / totalCount
+		q2 := (countSoFar + proposedCount) / totalCount
+		shouldAdd := z <= (q0*(1-q0)) && z <= (q2*(1-q2))
+		if shouldAdd {
+			centroids[cur].count += centroids[i].count
+			delta := centroids[i].mean - centroids[cur].mean
+			weightedDelta := (delta * centroids[i].count) / centroids[cur].count
+			centroids[cur].mean += weightedDelta
+		} else {
+			countSoFar += centroids[cur].count
+			cur++
+			centroids[cur] = centroids[i]
+		}
+		if cur != i {
+			centroids[i] = centroid{}
+		}
+	}
+	td.mu.numMerged = cur + 1
+	td.mu.mergedCount = totalCount
+	atomic.StoreInt64(&td.unmergedIdx, int64(td.mu.numMerged))
+}
+
+type centroids []centroid
+
+var _ sort.Interface = centroids(nil)
+
+func (c centroids) totalCount() float64 {
+	var sum float64
+	for i := range c {
+		sum += c[i].count
+	}
+	return sum
+}
+
+func (c centroids) Len() int           { return len(c) }
+func (c centroids) Less(i, j int) bool { return c[i].mean < c[j].mean }
+func (c centroids) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
 
 type config struct {
 	compression float64
